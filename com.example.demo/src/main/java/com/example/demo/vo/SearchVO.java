fafb19f06f5d00245e76bbc7085e3c6804fb660c
Lim HyunSeok <cr3ux53c@gmail.com>
2018-12-21 11:29:01 +0900

    db 쿼리 중

---
+++ com.example.demo/src/main/java/com/example/demo/vo/SearchVO.java
@@ -0,0 +1,25 @@
+package com.example.demo.vo;
+
+import java.util.Date;
+
+public class SearchVO {
+	private String searchText;
+	private Date searchDate;
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+
+	public Date getSearchDate() {
+		return searchDate;
+	}
+
+	public void setSearchDate(Date searchDate) {
+		this.searchDate = searchDate;
+	}
+
+}
